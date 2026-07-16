d801bd8b0adb002915ecd0a4ce84b77af868fb20
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:51:19 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,33 @@
 output "storage_data_lake_gen2_paths_id" {
   description = "Map of id values across all storage_data_lake_gen2_paths, keyed the same as var.storage_data_lake_gen2_paths"
-  value       = { for k, v in azurerm_storage_data_lake_gen2_path.storage_data_lake_gen2_paths : k => v.id }
+  value       = { for k, v in azurerm_storage_data_lake_gen2_path.storage_data_lake_gen2_paths : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "storage_data_lake_gen2_paths_ace" {
   description = "Map of ace values across all storage_data_lake_gen2_paths, keyed the same as var.storage_data_lake_gen2_paths"
-  value       = { for k, v in azurerm_storage_data_lake_gen2_path.storage_data_lake_gen2_paths : k => v.ace }
+  value       = { for k, v in azurerm_storage_data_lake_gen2_path.storage_data_lake_gen2_paths : k => v.ace if v.ace != null && length(v.ace) > 0 }
 }
 output "storage_data_lake_gen2_paths_filesystem_name" {
   description = "Map of filesystem_name values across all storage_data_lake_gen2_paths, keyed the same as var.storage_data_lake_gen2_paths"
-  value       = { for k, v in azurerm_storage_data_lake_gen2_path.storage_data_lake_gen2_paths : k => v.filesystem_name }
+  value       = { for k, v in azurerm_storage_data_lake_gen2_path.storage_data_lake_gen2_paths : k => v.filesystem_name if v.filesystem_name != null && length(v.filesystem_name) > 0 }
 }
 output "storage_data_lake_gen2_paths_group" {
   description = "Map of group values across all storage_data_lake_gen2_paths, keyed the same as var.storage_data_lake_gen2_paths"
-  value       = { for k, v in azurerm_storage_data_lake_gen2_path.storage_data_lake_gen2_paths : k => v.group }
+  value       = { for k, v in azurerm_storage_data_lake_gen2_path.storage_data_lake_gen2_paths : k => v.group if v.group != null && length(v.group) > 0 }
 }
 output "storage_data_lake_gen2_paths_owner" {
   description = "Map of owner values across all storage_data_lake_gen2_paths, keyed the same as var.storage_data_lake_gen2_paths"
-  value       = { for k, v in azurerm_storage_data_lake_gen2_path.storage_data_lake_gen2_paths : k => v.owner }
+  value       = { for k, v in azurerm_storage_data_lake_gen2_path.storage_data_lake_gen2_paths : k => v.owner if v.owner != null && length(v.owner) > 0 }
 }
 output "storage_data_lake_gen2_paths_path" {
   description = "Map of path values across all storage_data_lake_gen2_paths, keyed the same as var.storage_data_lake_gen2_paths"
-  value       = { for k, v in azurerm_storage_data_lake_gen2_path.storage_data_lake_gen2_paths : k => v.path }
+  value       = { for k, v in azurerm_storage_data_lake_gen2_path.storage_data_lake_gen2_paths : k => v.path if v.path != null && length(v.path) > 0 }
 }
 output "storage_data_lake_gen2_paths_resource" {
   description = "Map of resource values across all storage_data_lake_gen2_paths, keyed the same as var.storage_data_lake_gen2_paths"
-  value       = { for k, v in azurerm_storage_data_lake_gen2_path.storage_data_lake_gen2_paths : k => v.resource }
+  value       = { for k, v in azurerm_storage_data_lake_gen2_path.storage_data_lake_gen2_paths : k => v.resource if v.resource != null && length(v.resource) > 0 }
 }
 output "storage_data_lake_gen2_paths_storage_account_id" {
   description = "Map of storage_account_id values across all storage_data_lake_gen2_paths, keyed the same as var.storage_data_lake_gen2_paths"
-  value       = { for k, v in azurerm_storage_data_lake_gen2_path.storage_data_lake_gen2_paths : k => v.storage_account_id }
+  value       = { for k, v in azurerm_storage_data_lake_gen2_path.storage_data_lake_gen2_paths : k => v.storage_account_id if v.storage_account_id != null && length(v.storage_account_id) > 0 }
 }
 
